7e873695905313b84a63d5b9a860f938cf010a81
DenisR <den4ikgrandeoso@gmail.com>
2021-09-19 16:12:33 +0300

    Домашняя работа №8

---
+++ Д_з_8/Задание_3.py
@@ -0,0 +1,22 @@
+#Задание №3
+
+def ex_decorator(func):
+    def wrapper(*args, **kwargs):
+        for i in args:
+            print(f'{str(func).split()[1]}({i}: {type(i)})')
+        for i in kwargs:
+            print(f'{str(func).split()[1]}([ARG: {i}] = {kwargs[i]}: {type(kwargs[i])})')
+        return func(*args, **kwargs)
+
+    return wrapper
+
+@ex_decorator
+def function_with_arguments(a, b, c, d):
+    try:
+        b = int(b)
+        b = b ** 3
+    except:
+        b = 0
+    return b
+
+print(f'Выводим итоговый результат: {function_with_arguments(a="JOB", b=2, c=3.8, d={1: "2"})}')
